88bc32d7663f8904c170a08cd50fcf494e953876
Al Samir <shahinur1591@gmail.com>
2024-04-29 06:48:12 +0600

    Navbar changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,31 +26,76 @@ const Navbar = () => {
     });
   };
   return (
-    <div className="navbar py-4">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <>
+      <div className="navbar py-4">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className={`menu menu-sm dropdown-content mt-3 z-40 p-2 ${
+                isDarkMode ? "bg-dim-black" : "bg-white"
+              } shadow rounded-box w-52`}
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h7"
-              />
-            </svg>
+              <li>
+                <NavLink
+                  className={`${isDarkMode && "dark-mode-navlink"}`}
+                  to={`/`}
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className={`${isDarkMode && "dark-mode-navlink"}`}
+                  to={`/all-tourist-sports`}
+                >
+                  All Tourist Sports
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className={`${isDarkMode && "dark-mode-navlink"}`}
+                  to={`/add-tourist-sports`}
+                >
+                  Add Tourist Sports
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className={`${isDarkMode && "dark-mode-navlink"}`}
+                  to={`/my-list`}
+                >
+                  My List
+                </NavLink>
+              </li>
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className={`z-40 menu menu-sm dropdown-content mt-3 p-2 shadow ${
-              isDarkMode ? "bg-black" : "bg-white"
-            } rounded-box w-52`}
+          <a
+            className={`btn ${
+              isDarkMode ? "text-white" : "text-dim-black"
+            } btn-ghost lg:text-xl md:text-lg text-base`}
           >
+            Samir&apos;s Travel Agency
+          </a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className={`menu menu-horizontal px-1 gap-4`}>
             <li>
               <NavLink
                 className={`${isDarkMode && "dark-mode-navlink"}`}
@@ -85,73 +130,64 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
-      </div>
-      <div className="navbar-center">
-        <a
-          className={`btn ${
-            isDarkMode ? "text-white" : "text-dim-black"
-          } btn-ghost lg:text-xl md:text-lg text-base`}
-        >
-          Samir&apos;s Travel Agency
-        </a>
-      </div>
-      <div className="navbar-end">
-        {user ? (
-          <>
-            <Tooltip id="my-tooltip" className="z-10" />
+        <div className="navbar-end">
+          {user ? (
+            <>
+              <Tooltip id="my-tooltip" className="z-10" />
 
-            <a
-              data-tooltip-id="my-tooltip"
-              data-tooltip-content={user.displayName}
-              data-tooltip-place="top"
-            >
-              <img
-                src={user.photoURL}
-                alt={user.displayName}
-                className="w-10 rounded-full"
-              />
-            </a>
-            <button
-              onClick={singOut}
-              className={`btn btn-outline ${
-                isDarkMode ? "text-white" : "text-black"
-              } ml-4`}
-            >
-              Logout
-            </button>
-          </>
-        ) : (
-          <>
-            <Link to={`/login`}>
+              <a
+                data-tooltip-id="my-tooltip"
+                data-tooltip-content={user.displayName}
+                data-tooltip-place="top"
+              >
+                <img
+                  src={user.photoURL}
+                  alt={user.displayName}
+                  className="w-10 rounded-full"
+                />
+              </a>
               <button
+                onClick={singOut}
                 className={`btn btn-outline ${
                   isDarkMode ? "text-white" : "text-black"
                 } ml-4`}
               >
-                Login
+                Logout
               </button>
-            </Link>
-            <Link to={`/register`}>
-              <button
-                className={`btn btn-outline ${
-                  isDarkMode ? "text-white" : "text-black"
-                } ml-4`}
-              >
-                Register
-              </button>
-            </Link>
-          </>
-        )}
-
-        <button className="ml-4" onClick={toggleTheme}>
-          {isDarkMode ? (
-            <IoSunnyOutline className="text-xl text-white" />
+            </>
           ) : (
-            <IoMoon className="text-xl" />
+            <>
+              <Link to={`/login`}>
+                <button
+                  className={`btn btn-outline ${
+                    isDarkMode ? "text-white" : "text-black"
+                  } ml-4`}
+                >
+                  Login
+                </button>
+              </Link>
+              <Link to={`/register`}>
+                <button
+                  className={`btn btn-outline ${
+                    isDarkMode ? "text-white" : "text-black"
+                  } ml-4`}
+                >
+                  Register
+                </button>
+              </Link>
+            </>
           )}
-        </button>
+
+          <button className="ml-4" onClick={toggleTheme}>
+            {isDarkMode ? (
+              <IoSunnyOutline className="text-xl text-white" />
+            ) : (
+              <IoMoon className="text-xl" />
+            )}
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
