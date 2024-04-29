327cbc50e6f15d653a14655f7f9775d83c2a5c0c
Al Samir <shahinur1591@gmail.com>
2024-04-29 23:48:37 +0600

    Sort func done

--- src/Pages/AllTouristSport.jsx
+++ src/Pages/AllTouristSport.jsx
@@ -1,9 +1,11 @@
+import { ThemeContext } from "../ContextProvider/ThemeContext";
 import TouristSportCard from "../components/TouristSportCard";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 const AllTouristSport = () => {
   const [touristSports, setTouristSports] = useState([]);
   const [loading, setLoading] = useState(true);
+  const { isDarkMode } = useContext(ThemeContext);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,18 +23,49 @@ const AllTouristSport = () => {
         setLoading(false);
       }
     };
-
     fetchData();
   }, []);
 
+  const fetchDataInAscendingOrder = () => {
+    fetch("http://localhost:3000/tourist-sports/ascendingOrder")
+      .then((res) => res.json())
+      .then((result) => {
+        setTouristSports(result);
+      })
+      .catch((err) => console.error(err));
+  };
+
   return (
     <>
-      <div className="container mx-auto px-4 mt-20 mb-60">
+      <div className="container mx-auto px-4 mt-12 mb-60">
         {loading && (
           <div className="text-center">
             <span className="loading loading-bars loading-lg"></span>
           </div>
         )}
+        <div className="text-center mb-12">
+          <div className={`dropdown `}>
+            <div
+              tabIndex={0}
+              role="button"
+              className={`btn btn-outline ${
+                isDarkMode ? "text-white" : "text-light-black"
+              } m-1`}
+            >
+              Sort Average Cost
+            </div>
+            <ul
+              tabIndex={0}
+              className={`dropdown-content ${
+                isDarkMode ? "bg-white text-black" : "bg-black text-white"
+              } z-40 menu p-2 shadow rounded-box w-52`}
+            >
+              <li>
+                <a onClick={fetchDataInAscendingOrder}>Ascending Order</a>
+              </li>
+            </ul>
+          </div>
+        </div>
         <div className="grid md:grid-cols-2 grid-cols-1 gap-12">
           {touristSports.map((touristSport, index) => (
             <TouristSportCard key={index} touristSport={touristSport} />
